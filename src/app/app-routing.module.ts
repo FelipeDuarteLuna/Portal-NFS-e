d3c510cad389139f194767365aa012def461b705
rupolo.andre <rupolo.andre@totvs.com.br>
2022-10-11 09:12:35 -0300

    Rotas Componente Configurar - start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { LoginComponent } from './login/login.component';
 import { NgModule, } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ConfigurarComponent } from './pages/configurar/ConfigurarComponent';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'configurar', component: ConfigurarComponent }
 
 ];
 
