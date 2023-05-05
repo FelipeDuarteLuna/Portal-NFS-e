cc24d50751973faa2bb5886803c9c825ac92f411
FelipeDuarteLuna <felipinho.luna@gmail.com>
2023-05-05 14:17:24 -0300

    Automação 100% do componente LoginComponent e ServiçoAuthService

--- src/app/login/auth.service.spec.ts
+++ src/app/login/auth.service.spec.ts
@@ -1,16 +1,54 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { from } from 'rxjs';
+import { AppRoutingModule } from '../app-routing.module';
 
-describe('AuthService', () => {
+describe( AuthService.name, () => {
+
   let service: AuthService;
+  const eventTrue =
+    {
+      "login": "ADMIN",
+      "password": "123456"
+    };
+
+  const eventFalse =
+    {
+      "login": "LUNA",
+      "password": "123456"
+    };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+
+      imports:  [RouterTestingModule, AppRoutingModule ]
+    });
+
     service = TestBed.inject(AuthService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it(`${ AuthService.prototype.checkLogin.name} - Validação do Login e Senha para entrar no Portal New NFS-e.`, () =>{
+
+    //const spy = spyOn(service, 'mostraMenuEmitter');
+    service.checkLogin(eventTrue);
+
+
+    expect(sessionStorage.getItem("User")).toEqual("ADMIN");
+  });
+
+  it(`${ AuthService.prototype.checkLogin.name} - Validação do Login e Senha para entrar Else.`, () =>{
+
+    //const spy = spyOn(service, 'mostraMenuEmitter');
+    service.checkLogin(eventFalse);
+
+
+    expect(sessionStorage.getItem("User")).toEqual("ADMIN");
+  });
+
 });
--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,28 +1,48 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from './auth.service';
 
-describe('LoginComponent', () => {
-  let component: LoginComponent;
+describe(LoginComponent.name, () => {
+
+  let app: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let service: AuthService;
+  const eventTrue =
+  {
+    "login": "ADMIN",
+    "password": "123456"
+  };
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+
+      imports: [ RouterTestingModule ],
+      providers: [ AuthService ],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    app = fixture.componentInstance;
+
+    service = TestBed.inject(AuthService);
+
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+
+  it(`${LoginComponent.prototype.goToLogin.name} - should create the app.` , () => {
+    expect(app).toBeTruthy();
   });
+
+  it(`${LoginComponent.prototype.goToLogin.name} GoToLogin() - Função pra validar Login.`,() =>{
+
+    const spy = spyOn(service, 'checkLogin');
+    app.goToLogin(eventTrue);
+    expect(spy).toHaveBeenCalled();
+  });
+
+
 });
